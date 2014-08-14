bfacd153f58cdb59adbbfdb0bb944ca071900ae0
moonman <moonman.ca@gmail.com>
2014-08-14 00:47:51 -0600

    New builds. Toolchain rebuilt.

--- trunk/libs/libz/zlib-1.2.6/zconf.h
+++ trunk/libs/libz/zlib-1.2.6/zconf.h
@@ -388,11 +388,11 @@ typedef uLong FAR uLongf;
    typedef Byte       *voidp;
 #endif
 
-#if 1    /* was set to #if 1 by ./configure */
+#ifdef HAVE_UNISTD_H    /* may be set to #if 1 by ./configure */
 #  define Z_HAVE_UNISTD_H
 #endif
 
-#if 1    /* was set to #if 1 by ./configure */
+#ifdef HAVE_STDARG_H    /* may be set to #if 1 by ./configure */
 #  define Z_HAVE_STDARG_H
 #endif
 
